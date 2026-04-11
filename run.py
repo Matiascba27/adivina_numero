f2cff499c6f10fba1e0f067da5adb59beaaaf1fe
Matiascba27 <matiasnunez1969@gmail.com>
2026-04-11 18:08:53 -0300

    modificacion de run.py

--- run.py
+++ run.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, session
+import app as juego
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+application.secret_key = 'sup3rs3cr3t0.2850!'
+
+# Ruta para iniciar un nuevo juego
+@application.route('/', methods = ['GET'])
+# Ruta para manejar las adivinanzas del usuario
 def inicio():
-   return  render_template('/index.html')
+
+   session['estado'] = juego.crear_juego() # Crear un nuevo juego y almacenar el estado en la sesión
+
+   return  render_template('/index.html') # Renderizar la plantilla HTML para el juego
 
 
 
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	application.run(debug=True)
